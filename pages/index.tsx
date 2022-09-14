b30d87a3afdb0809dcf136b19a89516ce7ed186a
MeiSaM25970 <meisam25970@gmail.com>
2022-09-14 10:10:14 +0430

    create new app

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Hello</h1>
+      <h2>I AM HERE</h2>
+      <h3>@meisam25970</h3>
+    </div>
+  );
+};
+
+export default Home;
